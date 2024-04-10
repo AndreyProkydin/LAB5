7d6ad9ee075b8de339d48d973f48e0d1f5f529e2
Andrey Prokudin <107585116+AndreyProkydin@users.noreply.github.com>
2024-04-10 22:35:05 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace laboratorna_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.bttnRes = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::laboratorna_4.Properties.Resources.Снимок_экрана_2024_04_09_231719;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(745, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ведіть послідовність символів";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(216, 244);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(0, 13);
+            this.lblRes.TabIndex = 2;
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(219, 175);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(100, 20);
+            this.txtBox.TabIndex = 3;
+            // 
+            // bttnRes
+            // 
+            this.bttnRes.Location = new System.Drawing.Point(28, 244);
+            this.bttnRes.Name = "bttnRes";
+            this.bttnRes.Size = new System.Drawing.Size(75, 23);
+            this.bttnRes.TabIndex = 4;
+            this.bttnRes.Text = "Ok";
+            this.bttnRes.UseVisualStyleBackColor = true;
+            this.bttnRes.Click += new System.EventHandler(this.bttnRes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bttnRes);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button bttnRes;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laboratorna_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bttnRes_Click(object sender, EventArgs e)
+        {
+            if(IsJavaIdentifier(txtBox.Text))
+            {
+                lblRes.Text = "Ця послідовність правильно\r\nзображає ідентифікатор мови Java";
+            }
+            else
+            {
+                lblRes.Text = "Ця послідовність не правильно\r\nзображає ідентифікатор мови Java";
+            }
+        }
+
+        bool IsJavaIdentifier(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            if (!char.IsLetter(input[0]) && input[0] != '_')
+                return false;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(input[i]) && input[i] != '_')
+                    return false;
+            }
+
+            if (IsReservedWord(input))
+                return false;
+
+            return true;
+        }
+
+        bool IsReservedWord(string input)
+        {
+            string[] reservedWords = { "abstract", "assert", "boolean", "break", "byte", "case", "catch",
+                                   "char", "class", "const", "continue", "default", "do", "double",
+                                   "else", "enum", "extends", "final", "finally", "float", "for",
+                                   "goto", "if", "implements", "import", "instanceof", "int", "interface",
+                                   "long", "native", "new", "package", "private", "protected", "public",
+                                   "return", "short", "static", "strictfp", "super", "switch", "synchronized",
+                                   "this", "throw", "throws", "transient", "try", "void", "volatile", "while" };
+
+            return Array.IndexOf(reservedWords, input) != -1;
+        }
+    }
+}
